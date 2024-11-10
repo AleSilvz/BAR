9cb871b5127923692b446fa860a19142ab2a4921
AleSilvz <ale.silvzxs@gmail.com>
2024-11-10 20:58:15 -0300

    Add files via upload

---
+++ itens.js
@@ -0,0 +1,25 @@
+// itens.js
+
+const itens = [
+    { nome: "Coca-Cola", preco: 5.00 },
+    { nome: "Skol Lata", preco: 7.00 },
+    { nome: "Pepsi", preco: 5.50 },
+    { nome: "Guaraná Antarctica", preco: 4.50 },
+    { nome: "Heineken", preco: 8.00 },
+    { nome: "Frango", preco: 20.00},
+    { nome: "E", preco: 50.00},
+    { nome: "D", preco: 40.00},
+    { nome: "C", preco: 30.00},
+    { nome: "B", preco: 10.00},
+    { nome: "C", preco: 30.00},
+    { nome: "B", preco: 10.00},
+    { nome: "C", preco: 30.00},
+    { nome: "B", preco: 10.00},
+    { nome: "C", preco: 30.00},
+    { nome: "B", preco: 10.00},
+    { nome: "C", preco: 30.00},
+    { nome: "B", preco: 10.00},
+    { nome: "A", preco: 5.00}
+    // Adicione mais itens conforme necessário
+  ];
+  
